480d6ad205f5608b46e305964c33863308e7d49a
Artem Shiliaev <shiliaev@statmath.de>
2020-01-17 14:19:14 +0100

    cors problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,32 +3,25 @@ import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 import {ApolloProvider} from '@apollo/react-hoc';
-import {GetProjectComponent, withGetProject} from './api/interfaces/types.d';
+import {GetProjectComponent} from './api/interfaces/types.d';
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
     cache,
     link: new HttpLink({
         uri: 'http://localhost:8080/graphql',
-    })
+    }),
 });
 
 const App: React.FC = () => {
-    console.log(withGetProject());
     return (
         <ApolloProvider client={client}>
             <GetProjectComponent skip={false}>
                 {({loading, error, data}) => {
-                    if (loading) {
-                        return (
-                            <div>
-                                Loading...
-                            </div>
-                        )
-                    }
-                    return error
-                        ? (<div>{JSON.stringify(error)}</div>)
-                        : (<div>this is data: {data}</div>)
+                    console.log({loading, error, data});
+                    if (loading) return <p>Loading...</p>;
+                    if (error) return <p>Error :(</p>;
+                    return (<div>this is data: {data}</div>)
                 }}
             </GetProjectComponent>
         </ApolloProvider>
