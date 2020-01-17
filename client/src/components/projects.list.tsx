fbbb2dd72da5133573304ff7647db31258244866
Artem Shiliaev <shiliaev@statmath.de>
2020-01-17 17:54:17 +0100

    on click example

--- client/src/components/projects.list.tsx
+++ client/src/components/projects.list.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const ProjectsList = ({data}) => {
 
+    const onProjectClick = (project) => {
+        console.log(project)
+    };
+
     return (
         <div>
             <ul>
                 {data.queryProject.map(project => {
                     return (
-                        <li key={project.id}>
+                        <li key={project.id} onClick={() => onProjectClick(project)}>
                             <h5>Title: {project.title}</h5>
                             <h6>Tasks: {project.tasks.length}</h6>
                             <h6>Tags: {project.tags.length}</h6>
