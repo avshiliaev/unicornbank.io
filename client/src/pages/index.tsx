68b809ba3e6ac63e24a406ad9e1935d4ee496917
Artem Shiliaev <shiliaev@statmath.de>
2020-01-21 21:30:10 +0100

    project detail

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import {RouteComponentProps, Router} from '@reach/router';
 import ProjectsPage from './projects.page';
-
+const ProjectDetail = props => <h2>{props.id}</h2>;
 const Pages = () => {
     const RouterPage = (
       props: { pageComponent: JSX.Element } & RouteComponentProps
@@ -10,6 +10,7 @@ const Pages = () => {
         <Fragment>
             <Router primary={false} component={Fragment}>
                 <RouterPage path="/" pageComponent={<ProjectsPage/>}/>
+                <ProjectDetail path="/projects/:id"/>
             </Router>
         </Fragment>
     );
--- client/src/pages/projects.page.tsx
+++ client/src/pages/projects.page.tsx
@@ -5,37 +5,57 @@ import {WrappedProjectsAdd} from '../containers/projects.add';
 
 const {Header, Content, Footer} = Layout;
 
+const ProjectsOverviewHeader = () => {
+    return (
+        <Header>
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['1']}
+                style={{lineHeight: '64px'}}
+            >
+                <Menu.Item key="1">My projects</Menu.Item>
+                <Menu.Item key="2">Discover</Menu.Item>
+            </Menu>
+        </Header>
+    )
+};
+
+const ProjectsOverviewContent = () => {
+    return (
+        <Content style={{padding: '50px'}}>
+            <Row>
+                <Col span={8}>
+                    <div style={
+                        {background: '#fff', padding: 24, minHeight: 280}
+                    }>
+                        <WrappedProjectsAdd/>
+                    </div>
+                </Col>
+                <Col span={16}>
+                    <div style={
+                        {background: '#fff', padding: 24, minHeight: 280, marginLeft: '50px'}
+                    }>
+                        <ProjectsQuery/>
+                    </div>
+                </Col>
+            </Row>
+        </Content>
+    )
+};
+
+const ProjectsOverviewFooter = () => {
+    return (
+        <Footer style={{textAlign: 'center'}}>Lagerist ©2020</Footer>
+    )
+};
+
 const ProjectsPage = () => {
     return (
         <Layout style={{minHeight: '100vh'}}>
-            <Header>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['1']}
-                    style={{lineHeight: '64px'}}
-                >
-                    <Menu.Item key="1">My projects</Menu.Item>
-                    <Menu.Item key="2">Discover</Menu.Item>
-                </Menu>
-            </Header>
-            <Content style={{padding: '50px'}}>
-                <Row>
-                    <Col span={8}>
-                        <div style={{background: '#fff', padding: 24, minHeight: 280}}>
-                            <WrappedProjectsAdd/>
-                        </div>
-
-                    </Col>
-                    <Col span={16}>
-                        <div style={{background: '#fff', padding: 24, minHeight: 280, marginLeft: '50px'}}>
-                            <ProjectsQuery/>
-                        </div>
-                    </Col>
-                </Row>
-
-            </Content>
-            <Footer style={{textAlign: 'center'}}>Lagerist ©2020</Footer>
+            <ProjectsOverviewHeader/>
+            <ProjectsOverviewContent/>
+            <ProjectsOverviewFooter/>
         </Layout>
     )
 };
