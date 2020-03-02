73eca3dd4ca2d182288dbaba03c17db2c7b883ee
Artem Shiliaev <shiliaev@statmath.de>
2020-03-02 12:32:11 +0100

    project stats

--- client/src/components/project.detail.view.tsx
+++ client/src/components/project.detail.view.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import FlexGridBasic from '../layout/flex.grid.basic';
 import TasksTable from './tasks.table';
+import FlexContainer from './flex.container';
+import {Col, Row, Statistic} from 'antd';
 
 const ProjectTitle = ({project}) => {
     return (
-        <div>{project.title}</div>
+        <FlexContainer justify='center' align='center'>
+            <h2>{project.title}</h2>
+        </FlexContainer>
     )
 };
 
 const ProjectDescription = ({project}) => {
     return (
-        <div>
-            <div>Tags: {project.tags.length}</div>
-            <div>Tasks: {project.tasks.length}</div>
-            <div>Team: {project.workers.length}</div>
-        </div>
+        <Row gutter={16}>
+            <Col span={8}>
+                <Statistic title="Team: " value={project.workers.length}/>
+            </Col>
+            <Col span={8}>
+                <Statistic title="Tasks: " value={project.tasks.length}/>
+            </Col>
+            <Col span={8}>
+                <Statistic title="Topics: " value={project.tags.length}/>
+            </Col>
+        </Row>
     )
 };
 
