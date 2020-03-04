1bd86d50b6f50b1227d6c196548cafae47ea1367
Artem Shiliaev <shiliaev@statmath.de>
2020-03-04 10:00:22 +0100

    login state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import {ApolloProvider} from '@apollo/react-common';
 import Pages from './pages';
 import './App.css';
 import {useMedia} from 'react-use-media';
+import gql from 'graphql-tag';
+import {useQuery} from '@apollo/react-hooks';
+import {typeDefs} from './resolvers';
 
 /**
  * Updating cache strategies:
@@ -17,11 +20,48 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({
     cache,
     link: new HttpLink({
+        // TODO: add env var
         uri: 'http://localhost:8080/graphql',
+        // headers: {
+        //     authorization: localStorage.getItem('token'),
+        //     'client-name': 'Space Explorer [web]',
+        //     'client-version': '1.0.0',
+        // },
     }),
+    typeDefs,
 });
 
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+/**
+ * Render our app
+ * - We wrap the whole app with ApolloProvider, so any component in the app can
+ *    make GraphqL requests. Our provider needs the client we created above,
+ *    so we pass it as a prop
+ * - We need a router, so we can navigate the app. We're using Reach router for this.
+ *    The router chooses between which component to render, depending on the url path.
+ *    ex: localhost:3000/login will render only the `Login` component
+ */
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn({windowSize}) {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages windowSize={windowSize}/> : (<div>Login</div>);
+}
+
 const App: React.FC = () => {
+
+    localStorage.setItem('token', 'awesomeToken');
 
     const breakpoints = {
         xs: '480px',
@@ -47,7 +87,7 @@ const App: React.FC = () => {
 
     return (
         <ApolloProvider client={client}>
-            <Pages windowSize={windowSize}/>
+            <IsLoggedIn windowSize={windowSize} />
         </ApolloProvider>
     );
 };
