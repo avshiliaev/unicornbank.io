fcfe7ffab6e3b899acce06e070eee63a0d9203db
Artem Shiliaev <shiliaev@statmath.de>
2020-03-06 17:05:04 +0100

    sider mini title

--- client/src/components/layout/sider.basic.tsx
+++ client/src/components/layout/sider.basic.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Layout, Menu} from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import {Breadcrumb, Layout, Menu} from 'antd';
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
 
 const {Sider} = Layout;
 const {SubMenu} = Menu;
@@ -8,57 +8,60 @@ const {SubMenu} = Menu;
 const SiderBasic = () => {
 
     return (
-      <Sider width={200} className="site-layout-background">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <UserOutlined />
+        <Sider width={200} className="site-layout-background">
+            <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{height: '100%', borderRight: 0}}
+            >
+                <Breadcrumb style={{margin: '16px'}}>
+                    <Breadcrumb.Item>Title</Breadcrumb.Item>
+                </Breadcrumb>
+                <SubMenu
+                    key="sub1"
+                    title={
+                        <span>
+                <UserOutlined/>
                 subnav 1
               </span>
-            }
-          >
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <LaptopOutlined />
+                    }
+                >
+                    <Menu.Item key="1">option1</Menu.Item>
+                    <Menu.Item key="2">option2</Menu.Item>
+                    <Menu.Item key="3">option3</Menu.Item>
+                    <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu
+                    key="sub2"
+                    title={
+                        <span>
+                <LaptopOutlined/>
                 subnav 2
               </span>
-            }
-          >
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub3"
-            title={
-              <span>
-                <NotificationOutlined />
+                    }
+                >
+                    <Menu.Item key="5">option5</Menu.Item>
+                    <Menu.Item key="6">option6</Menu.Item>
+                    <Menu.Item key="7">option7</Menu.Item>
+                    <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu
+                    key="sub3"
+                    title={
+                        <span>
+                <NotificationOutlined/>
                 subnav 3
               </span>
-            }
-          >
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
+                    }
+                >
+                    <Menu.Item key="9">option9</Menu.Item>
+                    <Menu.Item key="10">option10</Menu.Item>
+                    <Menu.Item key="11">option11</Menu.Item>
+                    <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+            </Menu>
+        </Sider>
     )
 };
 
