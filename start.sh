89fa147fe1dd3937cb75641567c48d15d414631c
Artem Shiliaev <shiliaev@statmath.de>
2020-03-30 09:10:56 +0200

    adjusted start script

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ docker run -d -it -p 8080:8080 dgraph/standalone:v2.0.0-rc1
 sleep 20
 jq -n --arg schema "$(cat types.graphql)" '{ query: "mutation updateGQLSchema($sch: String!) { updateGQLSchema(input: { set: { schema: $sch }}) { gqlSchema { schema } } }", variables: { sch: $schema }}' | curl -X POST -H "Content-Type: application/json" http://localhost:8080/admin -d @- | jq -r
 sleep 5
-npm run generate
+npm run zeus
 sleep 2
 bash populate.sh
 
