cdcc890f19872ace88fa1941031f7a6e1faba2e7
Artem Shiliaev <shiliaev@statmath.de>
2020-06-16 12:28:17 +0200

    mock data for frontend

--- client/src/services/account.detail.service.ts
+++ client/src/services/account.detail.service.ts
@@ -4,17 +4,55 @@ const url = 'http://localhost:8080/accounts';
 
 const queryAccount = async (accountId: string): Promise<AccountInterface> => {
 
-  const payload: AccountInterface = {
-    id: accountId,
-    title: 'test',
-    description: 'test',
-    transactions: [
-      {
-        id: 'id',
-        title: 'test',
-      },
-    ],
-  };
+  const data: AccountInterface[] = [
+    {
+      id: '4rvdbt4',
+      title: 'DE4354 5465 4556 5423',
+      description: 'This is my primary account',
+      transactions: [
+        {
+          id: 'age43gbtd',
+          title: '-54.03',
+        },
+        {
+          id: 'zndfbt56hn',
+          title: '-435.99',
+        },
+      ],
+    },
+    {
+      id: '4tggbefrtid',
+      title: 'DE5247 8796 5231 1114',
+      description: 'Savings',
+      transactions: [
+        {
+          id: '546jtzht6h',
+          title: '+58.00',
+        },
+        {
+          id: '3243546htznrbtrevf4',
+          title: '+654.00',
+        },
+        {
+          id: '43545t35t',
+          title: '+10.00',
+        },
+      ],
+    },
+    {
+      id: '65u7ukzmtz',
+      title: 'DE8204 5674 4789 1568',
+      description: 'Deposit',
+      transactions: [
+        {
+          id: '45tz465jutn',
+          title: '+1000.00',
+        },
+      ],
+    },
+  ];
+
+  const payload: AccountInterface = data.find(acc => acc.id == accountId)
 
   return Promise.resolve(payload);
 };
--- client/src/services/accounts.overview.service.ts
+++ client/src/services/accounts.overview.service.ts
@@ -4,55 +4,55 @@ const url = 'http://localhost:8080/graphql';
 
 const queryAccounts = async (userId: string): Promise<AccountInterface[]> => {
 
-  const payload: AccountInterface[] = [
+  const data: AccountInterface[] = [
     {
-      id: 'id',
-      title: 'test',
-      description: 'test',
+      id: '4rvdbt4',
+      title: 'DE4354 5465 4556 5423',
+      description: 'This is my primary account',
       transactions: [
         {
-          id: 'id',
-          title: 'test',
+          id: 'age43gbtd',
+          title: '-54.03',
         },
         {
-          id: 'id',
-          title: 'test',
+          id: 'zndfbt56hn',
+          title: '-435.99',
         },
       ],
     },
     {
-      id: 'id',
-      title: 'test',
-      description: 'test',
+      id: '4tggbefrtid',
+      title: 'DE5247 8796 5231 1114',
+      description: 'Savings',
       transactions: [
         {
-          id: 'id',
-          title: 'test',
+          id: '546jtzht6h',
+          title: '+58.00',
         },
         {
-          id: 'id',
-          title: 'test',
+          id: '3243546htznrbtrevf4',
+          title: '+654.00',
         },
         {
-          id: 'id',
-          title: 'test',
+          id: '43545t35t',
+          title: '+10.00',
         },
       ],
     },
     {
-      id: 'id',
-      title: 'test',
-      description: 'test',
+      id: '65u7ukzmtz',
+      title: 'DE8204 5674 4789 1568',
+      description: 'Deposit',
       transactions: [
         {
-          id: 'id',
-          title: 'test',
+          id: '45tz465jutn',
+          title: '+1000.00',
         },
       ],
     },
   ];
 
-  return Promise.resolve(payload);
+  return Promise.resolve(data);
 };
 
 const addAccount = async (addAccountInput: AccountInterface): Promise<AccountInterface> => {
