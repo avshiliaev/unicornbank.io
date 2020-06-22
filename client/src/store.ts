70b331ede131b4aca63ecdba165a9ab2590aa69a
Artem Shiliaev <shiliaev@statmath.de>
2020-06-22 10:33:00 +0200

    refactored rootReducer

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,35 +1,16 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import authReducer from './reducers/auth.reducer';
-import { createResponsiveStateReducer, responsiveStoreEnhancer } from 'redux-responsive';
-import accountReducer from './reducers/account.reducer';
-import accountsOverviewReducer from './reducers/accounts.overview.reducer';
+import { responsiveStoreEnhancer } from 'redux-responsive';
 import { createReduxHistoryContext, reachify } from 'redux-first-history';
 import { createBrowserHistory } from 'history';
-import userReducer from './reducers/user.reducer';
 import createSagaMiddleware from 'redux-saga';
 import { getAccountsSaga } from './sagas/accounts.sagas';
+import rootReducer from './reducers';
 
-const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
+const { createReduxHistory, routerMiddleware } = createReduxHistoryContext({
   history: createBrowserHistory(),
 });
 const sagaMiddleware = createSagaMiddleware();
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-  accountsOverview: accountsOverviewReducer,
-  account: accountReducer,
-  user: userReducer,
-  router: routerReducer,
-  windowSize: createResponsiveStateReducer({
-    extraSmall: 480,
-    small: 576,
-    medium: 768,
-    large: 992,
-    extraLarge: 1200,
-  }),
-});
 
 const store = createStore(
   rootReducer,
