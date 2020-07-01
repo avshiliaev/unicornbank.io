6d64d0c4f50f36b4b819376bdbe07ccefcf72223
Artem Shiliaev <shiliaev@statmath.de>
2020-07-01 21:42:51 +0200

    first stream all ready

--- srv/streams/clientTr.js
+++ srv/streams/clientTr.js
@@ -1,15 +1,37 @@
 const WebSocket = require('ws')
 
 const ws = new WebSocket(
-  'ws://localhost:8082/streams/transactions?account=af6694ef-e84b-4a61-b51a-89b394f3aec9'
+  'ws://localhost:8082/streams/transactions?account=0c9a391f-b32b-416e-a665-13fa0d612df6',
 )
+
+let state = []
 
 ws.on('close', function close () {
   console.log('disconnected')
 })
 
-ws.on('message', function incoming (data) {
-  let message = JSON.parse(data)
-  let doc = message.payload
-  console.log(doc.transactions.map(tr => tr.status))
+ws.on('message', async function incoming (data) {
+  let message = await JSON.parse(data)
+  let payload = message.payload.transactions
+
+  switch (message.type) {
+    case 'init': {
+      state = payload
+      break
+    }
+    case 'update': {
+      state = payload
+      break
+    }
+  }
+
+  const mockComponent = (state) => {
+    return state.length > 0 ? {
+      transactions: state.length,
+      balance: state.map(tr => tr.amount).reduce(
+        (a, b) => a + b,
+      ),
+    } : 'Nothing here yet...'
+  }
+  console.log(mockComponent(state))
 })
